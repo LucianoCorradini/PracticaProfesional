0a0f4e6b904da99d256eb73aab03494ca7dbe6b5
nicosarubbi <nicosarubbi@gmail.com>
2012-03-30 09:45:32 -0300

    Add GPLv3

---
+++ pp/__init__.py
@@ -0,0 +1,22 @@
+"""
+    
+    Trabajo de Práctica Profesional
+    Lic. en Sistemas de Información
+    Universidad Nacional de Luján.
+    
+    Copyright (C) 2012  --
+                Nicolas Sarubbi  -- nicolas@sarubbi.com.ar
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, version 3 of the License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see http://www.gnu.org/licenses/gpl-3.0.html
+
+"""
