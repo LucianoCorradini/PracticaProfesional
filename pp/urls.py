c5d6dc5c4f4b8f52275757e88b256690cdf36eed
nicosarubbi <nicosarubbi@gmail.com>
2012-04-02 15:49:52 -0300

    Fixed utf-8

--- pp/urls.py
+++ pp/urls.py
@@ -4,16 +4,11 @@ from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-from testHomeApp import views
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', views.index),
-    # url(r'^pp/', include('pp.foo.urls')),
+    url(r'^$', views.index),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
+
