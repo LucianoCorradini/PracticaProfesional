54b81d478cee4cd0c595c66a20c2e64897f0b158
Brian Blanque <bblanque@gmail.com>
2012-04-28 00:23:46 -0300

    Added admin forms, modified M2M models.

--- pp/app/models.py
+++ pp/app/models.py
@@ -11,157 +11,154 @@ ESTADOS_RESERVA = (
 
 class Persona(models.Model):
 
-    id = models.AutoField(primary_key=True)
-    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
-    # relacion 1 a 1, 0-1, 1-0 chequear
+    user = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL)
     apellido = models.CharField(max_length=50)
     nombre = models.CharField(max_length=50)
-    cuil = models.IntegerField()
+    tipo_documento = models.CharField(max_length=5)
+    numero_documento = models.PositiveIntegerField()
+    fecha_nacimiento = models.DateField()#habra que validar
+    cuil = models.PositiveIntegerField()
+    empresa = models.CharField(max_length=50, null=True, blank=True)
     email = models.EmailField()
-    empresa = models.CharField(max_length=50)
-    fecha_nacimiento = models.DateField()
-    numero_documento = models.IntegerField()
-    #le pongo a todo 50 aunque seguramente el max length telefonos y tipo documento sea menor
-    tipo_documento = models.CharField(max_length=50)
-    telefono1 = models.CharField(max_length=50)
-    telefono2 = models.CharField(max_length=50)
+    telefono1 = models.CharField(max_length=20)
+    telefono2 = models.CharField(max_length=20, null=True, blank=True)
     #faltaria agregar el unique "numero_documento + tipo_documento"
 
     def __unicode__(self):
         return u'Persona: %s, %s\n Documento: %s, %i\n' % (self.apellido, self.nombre, self.tipo_documento, self.numero_documento)
 
 
+class Servicio(models.Model):
+
+    capacidad = models.PositiveIntegerField()
+    duracion_maxima = models.PositiveIntegerField()
+    duracion_minima = models.PositiveIntegerField()
+    entreturno = models.PositiveIntegerField()
+    es_habitacion = models.BooleanField()
+    nombre = models.CharField(max_length=50)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    tiempo_turno = models.IntegerField()
+    timeDelta = models.TimeField()#todavia no definido
+
+    def __unicode__(self):
+        return u'Servicio: %i, %s\n Precio: %f\n  Capacidad: %i\n Duracion: ( %i, %i, %i )\n' % (self.id, self.nombre, self.precio, self.capacidad, self.duracion_maxima,self.tiempo_turno, self.duracion_minima)
+
+
 class SolicitudReserva(models.Model):
 
-    id = models.AutoField(primary_key=True)
     cliente = models.ForeignKey(Persona, on_delete=models.PROTECT)
     empresa = models.BooleanField()#tambien podria ser NullBooleanField
     estado = models.CharField(max_length=1, choices=ESTADOS_RESERVA)
     fecha_alta = models.DateField(auto_now=True)
     monto = models.DecimalField(max_digits=10, decimal_places=2)
+    alertas = models.ManyToManyField(User, through='Alerta')
+    #Hago que los comentarios no sean M2M por conflicto con alertas
+    #comentarios = models.ManyToManyField(User, through='ComentarioSolicitudReserva')
+    detalles = models.ManyToManyField(Servicio, through='DetalleSolicitudReserva')
+
 
     def __unicode__(self):
-        return u'Solicitud de Reserva Numero: %i\n Cliente: %i\n Fecha de Alta: %s\n Monto Abonado: %f\n Estado: %s\n' % (self.id, self.cliente, self.fecha_alta, self.monto, self.estado)
+        return u'Solicitud: %i\n Cliente: %i\n Alta: %s\n Monto Abonado: %f\n Estado: %s\n' % (self.id, self.cliente, self.fecha_alta, self.monto, self.estado)
 
 
 class Alerta(models.Model):
 
-    id = models.AutoField(primary_key=True)
-    reserva = models.ForeignKey(SolicitudReserva, on_delete=models.CASCADE)
-    usuario = models.ManyToManyField(User)
+    reserva = models.ForeignKey(SolicitudReserva)
+    usuario = models.ForeignKey(User)
     fecha_hora = models.DateTimeField()
     comentario = models.CharField(max_length=100)
 
     def __unicode__(self):
-        return u'Alerta Numero: %i\n  Usuario: %i\n  Reserva: %i\n  Fecha y Hora: %s\n  Comentario: %s\n' % (self.id, self.usuario, self.reserva, self.fecha_hora, self.comentario)
+        return u'Reserva: %i\n  Activacion: %s\n  Comentario: %s\n' % ( self.reserva, self.fecha_hora, self.comentario)
 
 
-class ComentarioReserva(models.Model):
+class ComentarioSolicitudReserva(models.Model):
 
-    id = models.AutoField(primary_key=True)
-    reserva = models.ForeignKey(SolicitudReserva, on_delete=models.CASCADE)
-    usuario = models.ManyToManyField(User)
+    reserva = models.ForeignKey(SolicitudReserva)
     comentario = models.TextField()
 
     def __unicode__(self):
-        return u'id: %i\n  Usuario: %i\n  Reserva: %i\n  Comentario: %s\n' % (self.id, self.usuario, self.reserva, self.comentario)
+        return u'id: %i\n  Reserva: %i\n  Comentario: %s\n' % (self.id, self.reserva, self.comentario)
 
-
-class Reserva(models.Model):
-
-    id = models.AutoField(primary_key=True)
-    cliente = models.ForeignKey(Persona, on_delete=models.PROTECT)
-    empresa = models.BooleanField()
-    fecha_alta = models.DateField()
-#no esta clara la relacion entre Reserva y SolicitudReserva , tendria que haber una FK de SolicitudReserva a Reserva?
-
-    def __unicode__(self):
-        return u'Reserva: %i\n  Cliente: %i\n  Fecha de alta: %s\n  Empresa: %s\n' % (self.id, self.cliente, self.fecha_hora, self.empresa)
-
-
-class Servicio(models.Model):
-
-    id = models.AutoField(primary_key=True)
-    capacidad = models.IntegerField()
-    duracion_maxima = models.TimeField()#Era int en modelo logico
-    duracion_minima = models.TimeField()#Era int en modelo logico
-    entreturno = models.TimeField()#Era int en modelo logico
-    es_habitacion = models.BooleanField()
-    nombre = models.CharField(max_length=50)
-    precio = models.DecimalField(max_digits=10, decimal_places=2)
-    tiempo_turno = models.TimeField()#Era int en modelo logico
-
-    def __unicode__(self):
-        return u'Servicio: %i, %s\n Precio: %10d.%02d\n  Capacidad: %i\n Tiempo del Turno: %s\n  Duracion Maxima: %s\n  Duracion Minima: %s\n  Entre Turno: %s\n Es Habitacion: %s\n  ' % (self.id, self.nombre, self.precio, self.capacidad, self.tiempo_turno, self.duracion_maxima, self.duracion_minima, self.entreturno, self.es_habitacion)
 
 
 class DetalleSolicitudReserva(models.Model):
 
-    id = models.AutoField(primary_key=True)
-    reserva = models.ForeignKey(SolicitudReserva,on_delete=models.CASCADE)
+    reserva = models.ForeignKey(SolicitudReserva,on_delete=models)
     servicio = models.ForeignKey(Servicio,on_delete=models.PROTECT)
 
     def __unicode__(self):
         return u'Detalle Solicitud Reserva: %i, Reserva: %i, Servicio: %i.\n' % (self.id, self.reserva, self.servicio)
 
 
+class ServicioDisponible(models.Model):
+
+    servicio = models.ForeignKey(Servicio,on_delete=models.PROTECT)
+    activo = models.BooleanField()
+    numero = models.PositiveIntegerField()
+
+    def __unicode__(self):
+        return u'Servicio: %s, %i, Activo: %s\n' % (self.servicio.nombre,self.numero, self.activo)
+
+
+class Reserva(models.Model):
+
+    cliente = models.ForeignKey(Persona, on_delete=models.PROTECT)
+    empresa = models.BooleanField()
+    fecha_alta = models.DateField()
+    detalles = models.ManyToManyField(ServicioDisponible, through='DetalleReserva')
+
+    def __unicode__(self):
+        return u'Reserva: %i\n  Cliente: %i\n  Alta: %s\n  Empresa: %s\n' % (self.id, self.cliente, self.fecha_hora, self.empresa)
+
+
 class ImagenServicio(models.Model):
 
-    id = models.AutoField(primary_key=True)
-    servicio = models.ForeignKey(Servicio,on_delete=models.PROTECT)
+    servicio = models.ForeignKey(Servicio)
     imagen_url = models.SlugField()
     descripcion = models.TextField()
 
     def __unicode__(self):
-        return u'Imagen del servicio: %i, Servicio: %i, URL: %i.\n Descripcion: %s\n' % (self.id, self.servicio, self.imagen_url, self.descripcion)
+        return u'Imagen del servicio: %i, Servicio: %i, URL: %i.\n' % (self.id, self.servicio, self.imagen_url)
 
 
-class ServicioDisponible(models.Model):
-
-    id = models.AutoField(primary_key=True)
-    servicio = models.ForeignKey(Servicio,on_delete=models.PROTECT)
-    activo = models.BooleanField()
-    numero = models.IntegerField()
-
-    def __unicode__(self):
-        return u'Servicio Disponible: %i, Servicio: %i, Activo: %s, Numero: %i\n' % (self.id, self.servicio, self.activo, self.numero)
-
-
-#en DetalleReserva supongo que tiene que estar relacionado con la reserva en si, lo cual lleva al cliente que la tiene y no tengo FK cliente
 class DetalleReserva(models.Model):
 
-    id = models.AutoField(primary_key=True)
     reserva = models.ForeignKey(Reserva,on_delete=models.PROTECT)
     servicio = models.ForeignKey(ServicioDisponible,on_delete=models.PROTECT)
-    fin = models.DateField() #falta especificar en el modelo logico
-    inicio = models.DateField() #falta especificar en el modelo logico
+    inicio = models.DateTimeField()
+    fin = models.DateTimeField()
     monto = models.DecimalField(max_digits=10, decimal_places=2)
     pagado = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __unicode__(self):
-        return u'Reserva: %i, Detalle Reserva: %i, Servicio: %i\n Inicio: %s, Finalizacion: %s\n Monto: %f, Pagado: %f\n' % (self.reserva, self.id, self.servicio, self.inicio, self.fin, self.monto, self.pagado)
+        return u'Reserva: %i, Servicio: %i\n Inicio: %s, Fin: %s\n Monto: %f, Pagado: %f\n' % (self.reserva, self.servicio, self.inicio, self.fin, self.monto, self.pagado)
 
 
 class Promocion(models.Model):
 
-    id = models.AutoField(primary_key=True)
     fecha_desde = models.DateField()
     fecha_hasta = models.DateField()
     paquete = models.BooleanField()
     solo_empresas = models.BooleanField()
+    detalles = models.ManyToManyField(Servicio, through='DetallePromocion')
 
     def __unicode__(self):
-        return u'Promocion: %i, Fecha de Comienzo: %s, Fecha de Finalizacion: %s, Paquete: %s, Solo Empresas: %s\n' % (self.id, self.fecha_desde, self.fecha_hasta, self.paquete, self.solo_empresas)
+        return u'Promocion: %i, Comienzo y Finalizacion: %s, %s\n' % (self.id, self.fecha_desde, self.fecha_hasta)
 
 
 class DetallePromocion(models.Model):
 
-    id = models.AutoField(primary_key=True)
-    promocion = models.ForeignKey(Promocion, on_delete=models.PROTECT)
-    servicio = models.ForeignKey(Servicio, on_delete=models.CASCADE)
-    duracion_maxima = models.TimeField()#Era int en modelo logico
-    duracion_minima = models.TimeField()#Era int en modelo logico
+    promocion = models.ForeignKey(Promocion)
+    servicio = models.ForeignKey(Servicio)
+    duracion_maxima = models.PositiveIntegerField()
+    duracion_minima = models.PositiveIntegerField()
     descuento = models.FloatField()
+    timeDelta = models.TimeField()#todavia no definido
 
     def __unicode__(self):
-        return u'Promocion: %i, Detalle: %i, Servicio: %i, Duracion Maxima: %s, Duracion Minima: %s, descuento: %f\n' % (self.promocion, self.id, self.servicio, self.duracion_maxima, self.duracion_minima, self.descuento)
+        return u'Promocion: %i, Servicio: %i, Duracion: (%i, %i), descuento: %f\n' % (self.promocion, self.servicio, self.duracion_maxima, self.duracion_minima, self.descuento)
+
+
+
+
