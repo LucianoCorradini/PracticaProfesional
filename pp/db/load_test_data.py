4e518a51e25efa195f5c28e4a1238b7211cc9fe1
Brian Blanque <bblanque@gmail.com>
2012-05-18 00:50:23 -0300

    added load_test_data.py & persona.csv

---
+++ pp/db/load_test_data.py
@@ -0,0 +1,79 @@
+#-*- coding: utf-8 -*-
+import sqlite3
+import csv
+import codecs
+from sys import exit
+
+
+#Al definir los .csv los datos que son string o date o time van entre '' y no ""
+#las PKs y FKs tienen _id detrás.
+
+
+class UTF8Recoder:
+    """
+    Iterator that reads an encoded stream and reencodes the input to UTF-8
+    """
+    def __init__(self, f, encoding):
+        self.reader = codecs.getreader(encoding)(f)
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        return self.reader.next().encode("utf-8")
+
+
+class UnicodeReader:
+    """
+    A CSV reader which will iterate over lines in the CSV file "f",
+    which is encoded in the given encoding.
+    """
+
+    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
+        f = UTF8Recoder(f, encoding)
+        self.reader = csv.reader(f, dialect=dialect, **kwds)
+
+    def next(self):
+        row = self.reader.next()
+        return [unicode(s, "utf-8") for s in row]
+
+    def __iter__(self):
+        return self
+
+
+def csv2db(cursor, tablename):
+    reader = UnicodeReader(open(("%s.csv") % (tablename), "rb"),
+                            csv.excel, 'utf-8')
+    fieldnames = reader.next()
+    fieldnamesString = ', '.join(fieldnames)
+    try:
+        for row in reader:
+            r = []
+            for data in row:
+                if data == '':
+                    r.append(u"'null'")
+                else:
+                    r.append(data)
+#            print(row, r)
+            rowString = ', '.join(r)
+#            print(("INSERT INTO app_%s (%s) VALUES (%s)" %
+#                            (tablename, fieldnamesString, rowString)))
+            cursor.execute(("INSERT INTO app_%s (%s) VALUES (%s)" %
+                            (tablename, fieldnamesString, rowString)))
+        return None
+    except csv.Error, e:
+        exit('file app_%s.csv, line %d: %s' % (tablename, reader.line_num, e))
+        return None
+
+
+##########
+##-Main-##
+##########
+connection = sqlite3.connect("../db.sqlite3")
+cursor = connection.cursor()
+#acá agregar tablas
+csv2db(cursor, 'persona')
+
+
+cursor.close()
+connection.commit()
