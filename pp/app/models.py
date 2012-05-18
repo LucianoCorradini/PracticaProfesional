c251c91c993ff27215e3d0da98765ffe70d1a9e8
nicosarubbi <nicosarubbi@gmail.com>
2012-05-18 12:18:42 -0300

    Update app/models.py

--- pp/app/models.py
+++ pp/app/models.py
@@ -2,15 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User
 
 ESTADOS_RESERVA = {
-    'En Espera': ['Atendida','Cancelada'],
-    'Atendida': ['Cancelada','Confirmada'],
-    'Cancelada': [],
-    'Confirmada': ['Cancelada','En Curso','Finalizada'],
-    'En Curso': ['Finalizada'],
-    'Finalizada': [],
+    'en espera': ['atendido','cancelado'],
+    'atendido': ['cancelado','confirmado'],
+    'cancelado': [],
+    'confirmado': ['cancelado','iniciado'],
+    'iniciado': ['finalizado','arribado','cancelado'],
+    'arribado': ['finalizado','sobrepasado'],
+    'sobrepasado': ['finalizado']
+    'finalizado': [],
 }
 
-DEFAULT_ESTADO_RESERVA = 'En Espera'
+DEFAULT_ESTADO_RESERVA = 'en espera'
 
 ENUMERADO_ESTADOS_RESERVA = [(x, x) for x in ESTADOS_RESERVA.keys()]
 
@@ -40,6 +42,38 @@ class Persona(models.Model):
     def __unicode__(self):
         return u'%s, %s.' % (self.apellido, self.nombre)
 
+class TipoHabitacion(models.Model):
+    nombre = models.CharField(max_length=50)
+    descripcion = models.TextField(default='')
+    capacidad = models.PositiveIntegerField(default=2)
+    imagen_url = models.SlugField()
+    activo = models.BooleanField(default=True)
+
+    def __unicode__(self):
+        return self.nombre
+
+
+class Habitacion(models.Model):
+    numero = models.PositiveIntegerField()
+    activo = models.BooleanField(default=True)
+
+    def __unicode__(self):
+        return self.numero
+
+
+class Turno(models.Model):
+    tiempo_turno = models.PositiveIntegerField() # en minutos
+    entreturno = models.PositiveIntegerField(default=0) # en minutos
+    minimo = models.PositiveIntegerField(null=True) # en cantidad de turnos
+    maximo = models.PositiveIntegerField(null=True) # en cantidad de turnos
+    precio = models.DecimalField(max_digits=10, decimal_places=2) # por turno
+    tipo_habitacion = models.ForeignKey(TipoHabitacion)
+
+
+class TurnoHabitacion(models.Model):
+    habitacion = models.ForeignKey(Habitacion, related_name='turnos')
+    turno = models.ForeignKey(Turno, related_name='habitaciones')
+
 
 class Reserva(models.Model):
     cliente = models.ForeignKey(Persona, on_delete=models.PROTECT)
@@ -59,107 +93,21 @@ class Reserva(models.Model):
         else:
             return False
 
-
-class Turno(models.Model):
-    tiempo_turno = models.DateTimeField()
-    minimo = models.PositiveIntegerField()
-    maximo = models.PositiveIntegerField()
-
-#    def __unicode__(self):
-#        return unicode(self.id)
-
-
+        
 class SolicitudReserva(models.Model):
     reserva = models.ForeignKey(Reserva, related_name='solicitudes')
-    turno = models.ForeignKey(Turno, related_name='solicitudes')
+    turno = model.ForeignKey(Turno, related_name='solicitudes')
     inicio = models.DateTimeField()
     fin = models.DateTimeField()
 
 
-class TipoHabitacion(models.Model):
-    nombre = models.CharField(max_length=50)
-    capacidad = models.PositiveIntegerField()
-    imagen_url = models.SlugField()
-    descripcion = models.TextField()
-
-    def __unicode__(self):
-        return unicode(self.nombre)
-
-
-class Caracteristica(models.Model):
-    nombre = models.CharField(max_length=50)
-    descripcion = models.TextField()
-
-    def __unicode__(self):
-        return unicode(self.nombre)
-
-
-class Habitacion(models.Model):
-    numero = models.PositiveIntegerField()
-    caracteristicas = models.ManyToManyField(Caracteristica)
-
-    def __unicode__(self):
-        return unicode(self.numero)
-
-
-class TipoServicio(models.Model):
-    nombre = models.CharField(max_length=50)
-    descripcion = models.TextField()
-    imagen = models.SlugField()
-
-    def __unicode__(self):
-        return unicode(self.nombre)
-
-
-class Servicio(models.Model):
-    numero = models.PositiveIntegerField()
-#    estado = models.CharField(max_length=1, choices=ESTADOS_SERVICIO)
-    estado = models.CharField(max_length=30)
-    tipo = models.ForeignKey(TipoServicio)
-
-    def __unicode__(self):
-        return u"%s %s" % (self.tipo.nombre, self.numero)
-
-
-class TurnoServicio(Turno):
-    servicio = models.ForeignKey(Servicio)
-
-#    def __unicode__(self):
-#        return
-
-
-class TurnoHabitacion(Turno):
-    habitacion = models.ForeignKey(Habitacion)
-
-#    def __unicode__(self):
-#        return
-
-
 class DetalleReserva(models.Model):
-    reserva = models.ForeignKey(Reserva)
-    fechahora = models.DateTimeField()  # de llegada perhaps??
-    turnos = models.PositiveIntegerField()  # pondria llegada y salida
+    reserva = models.ForeignKey(Reserva, related_name='solicitudes')
+    turno_habitacion = model.ForeignKey(TurnoHabitacion,
+        related_name='reservas')
+    inicio = models.DateTimeField()
+    fin = models.DateTimeField()
     monto = models.DecimalField(max_digits=10, decimal_places=2)
-
-#    def __unicode__(self):
-#        return
-
-
-class DetalleReservaHabitacion(DetalleReserva):
-    habitacion = models.ForeignKey(Habitacion)
-    turno = models.ForeignKey(TurnoHabitacion)
-
-#    def __unicode__(self):
-#        return
-
-
-class DetalleReservaServicio(DetalleReserva):
-    servicio = models.ForeignKey(Servicio)
-    turno = models.ForeignKey(TurnoServicio)
-
-#    def __unicode__(self):
-#        return
-
 
 
 
